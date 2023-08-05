78b8fc6dcda70ad72b3fa4ab85adf28522c58175
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-08-06 00:27:48 +0300

    new backend

--- src/redux/auth/operationsWithUser.js
+++ src/redux/auth/operationsWithUser.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'http://localhost:8000';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'http://localhost:8000';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
